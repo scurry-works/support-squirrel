e7be5f05c65f6e49893a1ab22897660422d624d9
Furmissile <squirrelmissile.mail@gmail.com>
2025-12-28 11:54:54 -0500

    improved flow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import scurrypy, os, dotenv
+import os, dotenv
 
 dotenv.load_dotenv()
 
@@ -12,11 +12,18 @@ WELCOME_CHANNEL_ID = 1440890422481129546
 ACORN_EMOJI_ID = 1400922547679264768
 MEMBER_ROLE_ID = 1046627142345170984
 
-from scurrypy import Client, EventTypes, EmbedField, EmbedFooter
+from scurrypy import (
+    Client, Intents, 
+    EventTypes, 
+    MessageCreateEvent, Message,
+    ReactionAddEvent, GuildMemberAddEvent,
+    MessagePart, Attachment,
+    EmbedPart, EmbedThumbnail, EmbedImage, EmbedField, EmbedFooter
+)
+
 from scurry_kit import (
     EmbedBuilder as E, 
     PrefixAddon,
-    HooksAddon,
     EventsAddon,
     GuildEmojiCacheAddon, BotEmojisCacheAddon,
     setup_default_logger
@@ -24,24 +31,30 @@ from scurry_kit import (
 
 logger = setup_default_logger()
 
-class MyBot(Client):
-    def __init__(self):
-        super().__init__(
-            token=TOKEN,
-            intents=scurrypy.Intents.set(
-                message_content=True, 
-                guild_members=True, 
-                guild_message_reactions=True,
-                guild_emojis_and_stickers=True
-            ),
-        )
+class BotUser:
+    """Quick helper for fetching bot user on startup."""
+    
+    def __init__(self, client: Client):
+        self.user = client.application(APPLICATION_ID)
 
-        self.bot_user = self.application(APPLICATION_ID)
+        client.add_startup_hook(self.init_bot_user)
 
-client = MyBot()
+    async def init_bot_user(self):
+        self.user = await self.user.fetch()
+
+client = Client(
+    token=TOKEN,
+    intents=Intents.set(
+        message_content=True, 
+        guild_members=True, 
+        guild_message_reactions=True,
+        guild_emojis_and_stickers=True
+    )
+)
+
+bot_user = BotUser(client)
 
 # addons
-hooks = HooksAddon(client)
 events = EventsAddon(client)
 prefixes = PrefixAddon(client, APPLICATION_ID, '!')
 
@@ -49,23 +62,18 @@ prefixes = PrefixAddon(client, APPLICATION_ID, '!')
 guild_emojis = GuildEmojiCacheAddon(client)
 bot_emojis = BotEmojisCacheAddon(client, APPLICATION_ID)
 
-@hooks.on_start
-async def on_start(bot: MyBot):
-    app = await bot.bot_user.fetch()
-    bot.bot_user = app.bot
-
 @prefixes.listen('rules')
-async def on_build_rules(bot: MyBot, msg: scurrypy.Message):
-    event: scurrypy.MessageCreateEvent = msg.context
+async def on_build_rules(bot: Client, msg: Message):
+    event: MessageCreateEvent = msg.context
 
     if event.author.id != OWNER_ID:
         return
     
     flaming_acorn = bot_emojis.get_emoji('flaming_acorn').mention
 
-    embed = scurrypy.EmbedPart(
-        author=E.user_author(bot.bot_user),
-        thumbnail=scurrypy.EmbedThumbnail("attachment://rules_book.png"),
+    embed = EmbedPart(
+        author=E.user_author(bot_user.user),
+        thumbnail=EmbedThumbnail("attachment://rules_book.png"),
         description="Before we get started, let's establish some rules.",
         fields=[
             EmbedField(f"{flaming_acorn} Respect",
@@ -82,39 +90,39 @@ async def on_build_rules(bot: MyBot, msg: scurrypy.Message):
     )
 
     attachments = [
-        scurrypy.Attachment('assets/rules_book.png', 'rules'),
-        scurrypy.Attachment('assets/alert.png', 'warn')
+        Attachment('assets/rules_book.png', 'rules'),
+        Attachment('assets/alert.png', 'warn')
     ]
 
     await msg.send(
-        scurrypy.MessagePart(
+        MessagePart(
             embeds=[embed], 
             attachments=attachments
         )
     )
 
 @prefixes.listen('verify')
-async def on_build_verify(bot: MyBot, msg: scurrypy.Message):
-    event: scurrypy.MessageCreateEvent = msg.context
+async def on_build_verify(bot: Client, msg: Message):
+    event: MessageCreateEvent = msg.context
 
     if event.author.id != OWNER_ID:
         return
     if event.channel_id != VERIFY_CHANNEL_ID:
         return
     
-    embed = scurrypy.EmbedPart(
+    embed = EmbedPart(
         title='Verify',
         author=E.user_author(bot.bot_user),
-        thumbnail=scurrypy.EmbedThumbnail("attachment://plus.png"),
+        thumbnail=EmbedThumbnail("attachment://plus.png"),
         description="By reacting to this message, you agree to the rules."
     )
 
     attachments=[
-        scurrypy.Attachment('assets/plus.png', 'plus')
+        Attachment('assets/plus.png', 'plus')
     ]
 
     resp = await msg.send(
-        scurrypy.MessagePart(
+        MessagePart(
             embeds=[embed],
             attachments=attachments
         )
@@ -125,7 +133,7 @@ async def on_build_verify(bot: MyBot, msg: scurrypy.Message):
     await resp_msg.add_reaction(guild_emojis.get_emoji(ACORN_EMOJI_ID))
 
 @events.listen(EventTypes.MESSAGE_REACTION_ADD)
-async def on_verify(bot: MyBot, event: scurrypy.ReactionAddEvent):
+async def on_verify(bot: Client, event: ReactionAddEvent):
 
     # if correct channel and emoji and NOT the bot...
     if event.channel_id != VERIFY_CHANNEL_ID:
@@ -146,31 +154,31 @@ async def on_verify(bot: MyBot, event: scurrypy.ReactionAddEvent):
     await guild.add_guild_member_role(event.user_id, MEMBER_ROLE_ID)
 
 @events.listen(EventTypes.GUILD_MEMBER_ADD)
-async def on_welcome(bot: MyBot, event: scurrypy.GuildMemberAddEvent):
+async def on_welcome(bot: Client, event: GuildMemberAddEvent):
     channel = bot.channel(WELCOME_CHANNEL_ID)
 
     acorn = bot_emojis.get_emoji('acorn').mention
     bullet = bot_emojis.get_emoji('bullet').mention
 
-    embed = scurrypy.EmbedPart(
+    embed = EmbedPart(
         author=E.user_author(bot.bot_user),
         title=f"Welcome, {event.user.username}!",
-        thumbnail=scurrypy.EmbedThumbnail("attachment://bookie.png"),
+        thumbnail=EmbedThumbnail("attachment://bookie.png"),
         description=f"""
             {bullet} Read the rules in <#1046640388456321126>
             {bullet} Verify in <#1440890196517326930>
             {bullet} Hope you enjoy the bot!
         """,
-        image=scurrypy.EmbedImage('attachment://welcome.gif')
+        image=EmbedImage('attachment://welcome.gif')
     )
 
     attachments=[
-        scurrypy.Attachment("assets/bookie.png", 'bookie'),
-        scurrypy.Attachment("assets/welcome.gif", 'welcome')
+        Attachment("assets/bookie.png", 'bookie'),
+        Attachment("assets/welcome.gif", 'welcome')
     ]
 
     await channel.send(
-        scurrypy.MessagePart(
+        MessagePart(
             content=f"{acorn} <@{event.user.id}> *has stumbled upon a community of squirrels!* {acorn}",
             embeds=[embed],
             attachments=attachments
